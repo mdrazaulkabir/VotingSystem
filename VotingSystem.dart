e00840b7cc6de15c0aec924c62b4ac93d081bc07
mdrazaulkabir <razaulkabirit@gmail.com>
2025-04-19 17:53:38 +0600

    this is second commit

---
+++ VotingSystem.dart
@@ -0,0 +1,134 @@
+import 'dart:ffi';
+import 'dart:io';
+
+void main(){
+  Map<String,int>candidates={};
+  while(true){
+    print("\n...Voting System Project...");
+    print("1.Add candidate");
+    print('2.View candiate');
+    print('3.Cast vote for candidate');
+    print("4.Voting result");
+    print('5.Find winner');
+    print("6.Exit");
+
+    print("Choose an option 1-6:");
+    int choose=int.parse(stdin.readLineSync()!);
+    if(choose==1){
+      addCandidate(candidates);
+
+    }
+    else if(choose==2){
+      viewCandiate(candidates);
+
+    }
+    else if(choose==3){
+      castCandidate(candidates);
+
+    }
+    else if(choose==4){
+      viewResult(candidates);
+
+    }
+    else if(choose==5){
+      findWinner(candidates);
+
+      
+    }
+    else if(choose==6){
+
+    }
+    else if(choose==7){
+      
+    }
+    else{
+      print("You choose the wrong try again!");
+      break;
+    }
+
+  }
+}
+
+void addCandidate(Map<String,int>candidates){
+  print("Enter voter name:");
+  String name=stdin.readLineSync()!;
+  if(name!=null&&name.isNotEmpty){
+    if(!candidates.containsKey(name)){
+      candidates[name]=0;
+      print("Candidate $name added successfully!");
+    }
+    else{
+      print("Candidate name: $name is already exited in voted");
+    }
+  }
+  else{
+    print("Invalid input.Try again!");
+  }
+}
+
+void viewCandiate(Map<String,int>candidates){
+  if(candidates.isEmpty){
+    print("There is not available candidate add first:");
+  }
+  print("List of candidate:");
+  for(var candiate in candidates.keys){
+    print("The candidate name is : ${candiate}");
+  }
+
+}
+
+void castCandidate(Map<String,int>candidates){
+  if(candidates.isEmpty){
+    print("There is not available candidate add first:");
+  }
+  print("Enter your name Cast vote for candidate ");
+  String? name=stdin.readLineSync()!;
+  if(candidates.containsKey(name)){
+    candidates[name]=candidates[name]!+1;
+    print("Successfully vote caste for candiate $name");
+  }
+  else{
+    print("Invalid candidate. Try again!");
+  }
+}
+
+void viewResult(Map<String,int>candidates){
+  if(candidates.isEmpty){
+    print("There is no result fist need to vote");
+  }
+  print("Voting result :");
+  for(var candidate in candidates.entries){
+    print("Name:${candidate.key} voted gets :${candidate.value}");
+  }
+
+}
+
+void findWinner(Map<String,int>candidates){
+  if(candidates.isEmpty){
+    print("No result availble because no one added and voted:");
+  }
+print("Winner Candidate:");
+
+
+
+
+
+  String winner=candidates.keys.first;
+  int heightVote=candidates[winner]!;
+  for(var candidate in candidates.entries){
+    if(candidate.value>candidates[winner]!){
+      winner=candidate.key;
+      heightVote=candidate.value;
+    }
+  }
+print("Candidate name: ${winner} voted get is:$heightVote");
+
+
+//// very very important that you can use this two line       //vvi
+// var heightVote=candidates.entries.reduce((a,b)=>a.value>=b.value? a:b);
+// print("Candidate name: ${heightVote.key} voted get is :${heightVote.value}");
+
+
+
+
+}
